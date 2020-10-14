bec2d4b93f511b602f616c000dbbf4e8d6872676
Daniele Alessandra <daniele.alessandra@eng.it>
2020-10-14 12:19:45 +0200

    Restore exports

--- public_api.ts
+++ public_api.ts
@@ -1,4 +1,27 @@
 export * from './src/app/modules/mrf-form/mrf-form.module';
+// Form Elements
+export * from './src/app/modules/mrf-form/mrf-form.component';
+export * from './src/app/modules/mrf-form/form-element/form-element.component';
+export * from './src/app/modules/mrf-form/form-element/tooltip-button/tooltip-button.component';
+export * from './src/app/modules/mrf-form/layout/repeatable-container/repeatable-container.component';
+export * from './src/app/modules/mrf-form/form-element/autocomplete-element/autocomplete-element.component';
+export * from './src/app/modules/mrf-form/form-element/combo-element/combo-element.component';
+export * from './src/app/modules/mrf-form/form-element/button-element/button-element.component';
+export * from './src/app/modules/mrf-form/form-element/checkbox-element/checkbox-element.component';
+export * from './src/app/modules/mrf-form/form-element/chips-element/chips-element.component';
+export * from './src/app/modules/mrf-form/form-element/html-element/html-element.component';
+export * from './src/app/modules/mrf-form/form-element/wysiwyg-element/wysiwyg-element.component';
+export * from './src/app/modules/mrf-form/form-element/text-area-element/text-area-element.component';
+export * from './src/app/modules/mrf-form/form-element/text-element/text-element.component';
+export * from './src/app/modules/mrf-form/form-element/select-boxes/select-boxes.component';
+export * from './src/app/modules/mrf-form/form-element/radio-element/radio-element.component';
+export * from './src/app/modules/mrf-form/form-element/map-element/map-element.component';
+export * from './src/app/modules/mrf-form/form-element/text-multilingua/text-multilingua.component';
+export * from './src/app/modules/mrf-form/form-element/date-element/date-element.component';
+export * from './src/app/modules/mrf-form/form-element/upload-element/upload-element.component';
+export * from './src/app/modules/mrf-form/form-element/read-only/read-only.component';
+
+
 // Models
 export * from './src/app/modules/mrf-form/shared/models/form-element.model';
 
@@ -23,3 +46,6 @@ export * from './src/app/modules/mrf-form/shared/components/snack-bar-custom/sna
 // Pipes
 export * from './src/app/modules/mrf-form/shared/pipes/string-to-html/string-to-html.pipe';
 export * from './src/app/modules/mrf-form/shared/pipes/translatable/translatable.pipe';
+
+// Directives
+export * from './src/app/modules/mrf-form/shared/directives/register-form-model/register-form-model.directive';
